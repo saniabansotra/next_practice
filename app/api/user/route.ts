5ae3cdd73f399bb41292f5f6a0b849ee198e8e4f
Sania Bansotra <110049595+saniabansotra@users.noreply.github.com>
2024-10-05 12:10:53 +0530

    message

---
+++ app/api/user/route.ts
@@ -0,0 +1,14 @@
+// ap/api/user/route.ts
+import { NextRequest } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+const client=new PrismaClient();
+export async function POST(req: NextRequest) {
+const body=await req.json();
+await client.user.create({
+  data:{
+    username:body.username,
+    password:body.password
+  }
+})
+  return Response.json({message:" Your are logged in successfully!"})
+}
